eb4dee00ccdb5a870a59348114bf028dac99fdfe
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-07-06 15:25:03 -0500

    adding albums table for my sql and refoectoring files for orginization

---
+++ src/albums_migration.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(100) NOT NULL,
+    name_record_name VARCHAR(100) NOT NULL,
+    release_date VARCHAR(10),
+    sales FLOAT UNSIGNED NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+
+
+
