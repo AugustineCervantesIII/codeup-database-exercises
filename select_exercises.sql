573aa6b16dbb7f2b246ba0f1a2d01b84f8b9c571
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-07-08 21:51:45 -0500

    practiced basic statements and made a correction on update exercise;

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,23 +1,31 @@
 USE codeup_test_db;
 
 
-/*get     what     from table         specific search info*/
 SELECT 'The name of all albums by Pink Floyd' AS 'Exercise 1';
+/*get     what                  from table        specific search info*/
 SELECT artist_name, record_name FROM albums WHERE artist_name = 'Pink Floyd';
+
 
 SELECT 'The year Sgt. Peppers Lonely Hearts Club Band was released' AS 'Exercise 2';
 SELECT release_date, artist_name FROM albums WHERE record_name = 'Sgt. Peppers Lonely Hearts Club Band';
 
+
 SELECT 'The genre for Nevermind' AS 'Exercise 3';
 SELECT artist_name, genre FROM albums WHERE record_name = 'Nevermind';
 
+
 SELECT 'Which albums were released in the 1990s' AS 'Exercise 4';
-SELECT artist_name, record_name FROM albums WHERE release_date LIKE '%199%';
--- between was used
+SELECT artist_name, record_name, release_date FROM albums WHERE release_date LIKE '%199%';
+# SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;-- between was used instead
+
 
 SELECT 'Which albums had less than 20 million certified sales' AS 'Exercise 5';
-SELECT record_name FROM albums WHERE sales < 20;
+SELECT record_name, sales FROM albums WHERE sales < 20;
+
 
 SELECT 'All the albums with a genre of "Rock"' AS 'Exercise 6';
 SELECT * FROM albums WHERE genre = 'Rock';
+# this shows list if has rock anywhere in genre and rock not case sensitive with % search
+# SELECT * FROM albums WHERE genre LIKE '%rock%';
+
 
--- update_exercises.sql
+++ update_exercises.sql
@@ -3,15 +3,18 @@ USE codeup_test_db;
 SELECT record_name AS 'All albums in the table' FROM albums;
 
 
-SELECT record_name AS 'albums released before 1980' from albums WHERE release_date < 1980;
+SELECT record_name, release_date AS 'Albums released before 1980' FROM albums WHERE release_date < 1980;
 
 
-SELECT record_name AS 'albums by Micheal Jackson' FROM artist_name = 'Micheal Jackson';
+SELECT record_name AS 'Albums by Micheal Jackson' FROM albums WHERE artist_name = 'Michael Jackson';
+
 
 UPDATE albums SET sales = sales * 10;
 SELECT * FROM albums;
 
+
 UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
+
 
 UPDATE albums SET artist_name = 'Peter Jackson' WHERE artist_name = 'Michael Jackson';
 SELECT * FROM albums;
